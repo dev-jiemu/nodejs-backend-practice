55f1abcee9b0140135fd53dfbf7d6c52821c5ac0
jiemu <rebianne03@naver.com>
2025-01-30 17:40:20 +0900

    2025.01.30 nodejs-backend-practice - config dist 경로이슈 fix

--- NestJS/user-practice/src/app.module.ts
+++ NestJS/user-practice/src/app.module.ts
@@ -7,12 +7,13 @@ import { ConfigModule } from '@nestjs/config';
 import emailConfig from './config/emailConfig';
 import { validationSchema } from './config/validationSchema';
 import { TypeOrmModule } from '@nestjs/typeorm'
+import * as path from 'node:path';
 
 // (process.env.NODE_ENV === 'production') ? '.production.env' : (process.env.NODE_ENV === 'stage') ? '.stage.env' : '.development.env'
 @Module({
     imports: [
         ConfigModule.forRoot({
-            envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+            envFilePath: [path.resolve(process.cwd(), `config/env/.${process.env.NODE_ENV}.env`)],
             load: [emailConfig],
             isGlobal: true,
             validationSchema, // joi 활용해서 유효성 검사
